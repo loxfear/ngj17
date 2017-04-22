e01178c1e65dfbb1148aca8d99c45fad9bb602a7
Jeff <jeff@gyldenbrand.dk>
2017-04-22 15:52:51 +0200

    programming shit 'n stuff

---
+++ NGJ17/Licens/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+	
+	public float points = 10;
+	private float score;
+	public GameObject itemOBJ;
+	private bool blah;
+
+
+
+
+
+	// Use this for initialization
+
+	void Start () {
+
+		itemOBJ = GameObject.FindWithTag ("itemOBJ");
+
+		//this.blah = GetComponent<pickItem> ();
+		}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+	//		hit.collider.gameObject.GetComponent<pickItem> ().isPicked ();
+
+			//pickItem blah = hit.collider.gameObject.GetComponent<pickItem> ();
+
+			Physics.Raycast (ray, out hit);
+			if (hit.collider.tag == "itemOBJ") {
+				if (hit.collider.GetComponent<pickItem> ().isPicked == true) {
+					AddPoints (points);
+				}
+			}
+		}
+	}
+
+	void OnGUI() {
+		GUILayout.Label("Score: " + score.ToString("0"));
+	}
+
+	void AddPoints(float points) {
+		score += points;
+	}
+}
---
+++ NGJ17/Licens/Assets/Scripts/pickItem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickItem : MonoBehaviour
+{	
+	//private bool isFarvet;
+	public bool isPicked = false;
+			
+
+	void OnMouseDown()
+	{
+		if (gameObject.GetComponent<Renderer>().material.color != Color.green) {
+			gameObject.GetComponent<Renderer> ().material.color = Color.green;
+
+			print ("farvede object");
+			isPicked = true;
+
+		} else {
+			print ("allerede farvet");
+			isPicked = false;
+		}
+
+	}
+
+
+}
+	
