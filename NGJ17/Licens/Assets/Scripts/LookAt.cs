807a5bebf9b34024f2f0cff31ca349c150907834
Joachim Gerber <joachim@bolverkgames.com>
2017-04-23 00:44:41 +0200

    apartment changes

---
+++ NGJ17/Licens/Assets/Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt: MonoBehaviour
+{
+    // Use this for initialization
+    public Transform target;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tempPos = new Vector3(target.position.x, 0, target.position.z); //this will only affect the y axis, you can shuffle the values around to make it only x or only z or a combination of any.
+        transform.LookAt(tempPos);
+    }
+}
