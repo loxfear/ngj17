178df8a1987919edb41a45d2cf2c0a24e1d6561e
Joachim Gerber <joachim@bolverkgames.com>
2017-04-23 11:10:03 +0200

    sounds implemented

---
+++ NGJ17/Licens/Assets/Scripts/PlaySoundsMia.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundsMia : MonoBehaviour {
+
+    public AudioClip[] gravel;
+    public AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (source.isPlaying) return;
+        source.clip = gravel[Random.Range(0, gravel.Length)];
+        source.Play();
+    }
+}
