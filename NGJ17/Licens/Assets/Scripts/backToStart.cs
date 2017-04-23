81882845ce8833b2471f641b7e1788ea3f9897d9
Jeff <jeff@gyldenbrand.dk>
2017-04-23 12:44:08 +0200

    fixed end screen, added new script

---
+++ NGJ17/Licens/Assets/Scripts/backToStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class backToStart : MonoBehaviour, IPointerClickHandler {
+
+
+
+
+		public void OnPointerClick(PointerEventData eventData) {
+			Application.LoadLevel("start_screen");
+		}
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
